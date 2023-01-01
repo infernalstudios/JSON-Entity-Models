409e98fa52ade6d5a7553ac0c4a21a63ec9a6ef4
Darien Gillespie <dsgillespie02@yahoo.com>
2023-01-01 17:01:14 -0500

    Added Mod class

---
+++ src/main/java/org/infernalstudios/jsonentitymodels/JSONEntityModels.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2022 Infernal Studios
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.infernalstudios.jsonentitymodels;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(JSONEntityModels.MOD_ID)
+public class JSONEntityModels {
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "jsonentitymodels";
+
+    public JSONEntityModels() {
+        final ModLoadingContext modLoadingContext = ModLoadingContext.get();
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    }
+}
