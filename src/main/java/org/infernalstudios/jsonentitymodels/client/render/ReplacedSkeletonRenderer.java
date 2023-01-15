4301f9c1ad308178dac45e26ee1344fd66b4dd28
Darien Gillespie <dsgillespie02@yahoo.com>
2023-01-15 16:06:52 -0500

    Fixed render type default to no cull

--- src/main/java/org/infernalstudios/jsonentitymodels/client/render/ReplacedSkeletonRenderer.java
+++ src/main/java/org/infernalstudios/jsonentitymodels/client/render/ReplacedSkeletonRenderer.java
@@ -1,9 +1,7 @@
 package org.infernalstudios.jsonentitymodels.client.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
@@ -21,12 +19,6 @@ public class ReplacedSkeletonRenderer extends ExtendedGeoReplacedEntityRenderer<
 
     public ReplacedSkeletonRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new ReplacedSkeletonModel(), new ReplacedSkeletonEntity());
-    }
-
-
-    @Override
-    public RenderType getRenderType(Object animatable, float partialTick, PoseStack poseStack, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, int packedLight, ResourceLocation texture) {
-        return RenderType.entityCutoutNoCull(texture);
     }
 
     @Override
