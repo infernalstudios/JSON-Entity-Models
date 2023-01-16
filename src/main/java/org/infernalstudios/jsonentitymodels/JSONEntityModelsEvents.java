3ee6e31bbbcc0b86d694b756f1ee09afd66b3e5e
Darien Gillespie <dsgillespie02@yahoo.com>
2023-01-16 16:40:48 -0500

    Implemented Defaults System

--- src/main/java/org/infernalstudios/jsonentitymodels/JSONEntityModelsEvents.java
+++ src/main/java/org/infernalstudios/jsonentitymodels/JSONEntityModelsEvents.java
@@ -1,30 +1,77 @@
 package org.infernalstudios.jsonentitymodels;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.infernalstudios.jsonentitymodels.client.model.ReplacedDefaultModel;
 import org.infernalstudios.jsonentitymodels.client.render.ReplacedCreeperRenderer;
+import org.infernalstudios.jsonentitymodels.client.render.ReplacedDefaultRenderer;
 import org.infernalstudios.jsonentitymodels.client.render.ReplacedIronGolemRenderer;
 import org.infernalstudios.jsonentitymodels.client.render.ReplacedSheepRenderer;
 import org.infernalstudios.jsonentitymodels.client.render.ReplacedSkeletonRenderer;
 import org.infernalstudios.jsonentitymodels.client.render.ReplacedSpiderRenderer;
 import org.infernalstudios.jsonentitymodels.client.render.ReplacedZombieRenderer;
+import org.infernalstudios.jsonentitymodels.entity.ReplacedDefaultEntity;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @Mod.EventBusSubscriber(modid = JSONEntityModels.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class JSONEntityModelsEvents {
+    private static final Map<EntityType<? extends Entity>, EntityRendererProvider<? extends Entity>> RENDERER_MAP = new HashMap();
+
+    static {
+        RENDERER_MAP.put(EntityType.CREEPER, ReplacedCreeperRenderer::new);
+        RENDERER_MAP.put(EntityType.SPIDER, ReplacedSpiderRenderer::new);
+        RENDERER_MAP.put(EntityType.ZOMBIE, ReplacedZombieRenderer::new);
+        RENDERER_MAP.put(EntityType.SKELETON, ReplacedSkeletonRenderer::new);
+        RENDERER_MAP.put(EntityType.IRON_GOLEM, ReplacedIronGolemRenderer::new);
+        RENDERER_MAP.put(EntityType.SHEEP, ReplacedSheepRenderer::new);
+
+        // TODO: Add 4 remaining mobs for Beta 1
+    }
+
+    private static Map<EntityType<?>, EntityRendererProvider<?>> DEFAULT_RENDERERS = new HashMap<>();
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
-            event.registerEntityRenderer(EntityType.CREEPER, ReplacedCreeperRenderer::new);
-            event.registerEntityRenderer(EntityType.SPIDER, ReplacedSpiderRenderer::new);
-            event.registerEntityRenderer(EntityType.ZOMBIE, ReplacedZombieRenderer::new);
-            event.registerEntityRenderer(EntityType.SKELETON, ReplacedSkeletonRenderer::new);
-            event.registerEntityRenderer(EntityType.IRON_GOLEM, ReplacedIronGolemRenderer::new);
-            event.registerEntityRenderer(EntityType.SHEEP, ReplacedSheepRenderer::new);
+        DEFAULT_RENDERERS = new HashMap<>(EntityRenderers.PROVIDERS);
+        replaceRenderers();
+    }
 
-            // TODO: Add remaining 4 mobs here to actually replace the renderers
+    public static void replaceRenderers() {
+        for (EntityType<?> entity : ForgeRegistries.ENTITIES) {
+            if (entity.getRegistryName().toString().equals("minecraft:player")) continue;
+
+            if (doesEntityHaveResource(entity)) {
+                ResourceLocation registryName = entity.getRegistryName();
+
+                EntityRenderers.register(entity, (EntityRendererProvider<Entity>) RENDERER_MAP.getOrDefault(entity, (context) -> new ReplacedDefaultRenderer(context,
+                    new ReplacedDefaultModel(registryName.getNamespace(), registryName.getPath()), new ReplacedDefaultEntity())));
+            } else {
+                EntityRenderers.register(entity, (EntityRendererProvider<Entity>) DEFAULT_RENDERERS.get(entity));
+            }
+
+        }
+    }
+
+    private static boolean doesEntityHaveResource(EntityType<?> entityType) {
+        ResourceLocation entityTypeRegistry = entityType.getRegistryName();
+        String resourceType = "geo/" + entityTypeRegistry.getNamespace() + "/" + entityTypeRegistry.getPath();
+
+        Collection<ResourceLocation> resources = Minecraft.getInstance().getResourceManager().listResources(resourceType,
+            (filename) -> filename.endsWith(".json"));
+
+        return resources.stream().anyMatch(resourceLocation -> resourceLocation.getNamespace().equals(JSONEntityModels.MOD_ID));
     }
 }
---
+++ src/main/java/org/infernalstudios/jsonentitymodels/client/model/ReplacedDefaultModel.java
@@ -0,0 +1,7 @@
+package org.infernalstudios.jsonentitymodels.client.model;
+
+public class ReplacedDefaultModel extends HeadTurningAnimatedGeoModel {
+    public ReplacedDefaultModel(String namespace, String entityName) {
+        super(namespace, entityName);
+    }
+}
